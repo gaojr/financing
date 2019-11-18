bc46949b6279105250d33331d901c4461c1e67f7
gaojr <gaojr427@hotmail.com>
2019-11-18 18:10:11 +0800

    修改金钱格式校验

--- gfinacing/js/index.js
+++ gfinacing/js/index.js
@@ -25,17 +25,29 @@ function checkDay(e) {
 }
 
 /**
- * 校验数字格式
+ * 校验金钱格式
  * @param {*} e 校验对象
  */
 function checkMoney(e) {
   // 清除"数字"和"."以外的字符
   e.value = e.value.replace(/[^\d.]/g, "");
-  // 验证第一个字符是非零数字
-  e.value = e.value.replace(/^([0.])/g, "");
   // 清除多余的"."
   e.value = e.value.replace(/\.{2,}/g, ".");
   e.value = e.value.replace(".", "$#$").replace(/\./g, "").replace("$#$", ".");
   // 只能输入两个小数
-  e.value = e.value.replace(/^(\-)*(\d+)\.(\d{2}).*$/, '$1$2.$3');
+  e.value = e.value.replace(/^(\-)*(\d+)\.(\d{2}).*$/, "$1$2.$3");
+  var isOk = true;
+  do {
+    isOk = true;
+    // 不能以"."开头
+    if (e.value.match(/^\./g)) {
+      e.value = "0" + e.value;
+      isOk = false;
+    }
+    // 大于1的数字不能以"0"开头
+    if (e.value.match(/^0\d+/g)) {
+      e.value = e.value.substring(1);
+      isOk = false;
+    }
+  } while (!isOk);
 }
